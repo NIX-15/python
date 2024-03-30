1287b7e63cd3b68e9527d1c3c026e01b4f33514c
Nikita <nix15@bk.ru>
2024-03-30 13:07:32 +0300

    sem2t3.2

--- sem2/task2/s2t2.py
+++ sem2/task2/s2t2.py
@@ -6,4 +6,4 @@ while fib<n:
   print(fib,i)
   fib,next_fib = next_fib,fib+next_fib
   i+=1
-print(f"Solved! {n} is #{i} in Fibonacci series" if fib == n else -1)
+print(f"Solved! {n} is #{i} in Fibonacci series" if fib == n else -1)#Тернарный оператор (Ternary), f-string
