b1ca11db31ba53e079191c698dfbd8ecf8e06efb
Nikita <nix15@bk.ru>
2024-03-30 12:36:42 +0300

    sem2t2final

--- sem2/task2/s2t2.py
+++ sem2/task2/s2t2.py
@@ -3,11 +3,7 @@ fib,next_fib = 0,1
 i=1
 print("f","i")
 while fib<n:
-  print(fib,i) 
+  print(fib,i)
   fib,next_fib = next_fib,fib+next_fib
   i+=1
-#print("Solved! ",n,"is #",i,"in Fibonacci series" if next_fib==n else -1)
-if fib==n:
-  print("Solved! ",n,"is #",i,"in Fibonacci series")
-else:
-  print(-1)
+print(f"Solved! {n} is #{i} in Fibonacci series" if fib == n else -1)
