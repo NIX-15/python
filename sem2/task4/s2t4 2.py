1ddd5606044d10dedfc6edda336a671ffce998cf
Nikita <nix15@bk.ru>
2024-03-30 13:23:19 +0300

    sem2t3f

--- sem2/task4/s2t4 2.py
+++ sem2/task4/s2t4 2.py
@@ -1,10 +1,14 @@
 import random
-max,min=0.0,10.0
+MAX_WEIGHT=1.0
+MIN_WEIGHT=10.0
+
+min_a=MAX_WEIGHT
+max_a=MIN_WEIGHT
 n=abs(int(input("How many watermelons?: ")))
-for i in range(n):
+for _ in range(n):
     # mass=abs(float(input("Enter the weight of watermelon")))
-    mass=round(random.uniform(1.0, 10.0),2)
-    print(mass)
-    if mass>max:max=mass
-    elif mass<min:min=mass
-print('min -',min,'max -',max)
+    mass=round(random.uniform(MIN_WEIGHT, MAX_WEIGHT),2)
+    print(mass, end=" ")
+    if mass>max_a:max_a=mass
+    elif mass<min_a:min_a=mass
+print(f"\n\nmin - {min_a}\nmax - {max_a}")
