ad2e7eb7f23ef589f8e40b35e5491ffeb5041397
Nikita <nix15@bk.ru>
2024-03-30 13:46:03 +0300

    sem2t4.1

--- sem2/task3/s2t3.py
+++ sem2/task3/s2t3.py
@@ -1,28 +1,15 @@
-flag=False
-while(not flag):
-    n=abs(int(input("Enter the number of days:")))
-    if(n>10):
-        print("error")
+import random
+MIN_TEMP=-50
+MAX_TEMP=50
+record = series = 0
+n=abs(int(input("Enter the number of days: ")))
+for i in range(n):
+    temp = random.randint(MIN_TEMP,MAX_TEMP)
+    print(f"Day {i+1} - Temperature {temp}")
+    if temp>0:
+        series+=1
+    elif series>record:
+        record,series=series,0
     else:
-        flag=True
-c=0
-max=0
-i=1
-while(i<=n):
-    while(flag):
-        t=int(input("Enter the temperature"))
-        if(t>50 or t<-50):
-            print("Temperature error!")
-        else:
-            flag=False
-    print("day",i,"temperature",t)
-    if(t>0):
-        c+=1
-        if(c>max):
-            max=c
-    else:
-        if(c>max):
-            c=0
-    i+=1
-    flag=True
-print(max)
+        series=0   
+print(record)
