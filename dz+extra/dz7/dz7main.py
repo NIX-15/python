1e9059fea54f10ce8ae8c6057d99e6908a9c6291
Nikita <nix15@bk.ru>
2024-07-16 22:03:26 +0300

    hw7_try_to_add_pointer

--- dz+extra/dz7/dz7main.py
+++ dz+extra/dz7/dz7main.py
@@ -1,12 +1,24 @@
 phone_book = "phone_book.txt"
-
-def check_phone_book_empty(file_name):
+pointer = ""
+def check_phone_book_empty(file_name,cursor):
     import os
     if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
         with open(file_name, 'r') as book_data:
             for line in book_data:
                 print(line)
     else:
-        print("Справочник пуст")
+        print("Справочник пуст!")
+        print("Выберите действие:\nadd - добавить контакт, exit - закрыть справочник")
+        cursor=input()
+        while(cursor != "add" and cursor != "exit"):
+            print("Error!")
+            cursor=input()
+        if(cursor == "add"):
+            print("Напишите имя!")
+        else:
+            print("Good Bye!")
+                
 
-check_phone_book_empty(phone_book)
+
+
+check_phone_book_empty(phone_book,pointer)
