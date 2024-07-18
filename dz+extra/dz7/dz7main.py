3b58dc38f9250152d1a0669859dc03f027c8b97f
Nikita <nix15@bk.ru>
2024-07-18 19:45:07 +0300

    hw7_try_to_add_addition_module

--- dz+extra/dz7/dz7main.py
+++ dz+extra/dz7/dz7main.py
@@ -1,5 +1,12 @@
+
 phone_book = "phone_book.txt"
 pointer = ""
+def addition():
+     new_contact = input("Напишите имя!: ")
+     new_contact = new_contact+input("Напишите телефон: ")
+     new_contact = new_contact+input("Напишите комментарий: ")
+     with open("phone_book.txt", 'a') as book_data:
+            book_data.write(new_contact)
 def check_phone_book_empty(file_name,cursor):
     import os
     if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
@@ -14,7 +21,8 @@ def check_phone_book_empty(file_name,cursor):
             print("Error!")
             cursor=input()
         if(cursor == "add"):
-            print("Напишите имя!")
+           addition()
+           check_phone_book_empty(phone_book,pointer)
         else:
             print("Good Bye!")
                 
