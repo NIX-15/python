5838d3a1b7f8d28e922ce50b9ace05f8a562d6ce
Nikita <nix15@bk.ru>
2024-08-22 17:39:30 +0300

    hw7_ modify_done

--- dz+extra/dz7/functions_phonebook.py
+++ dz+extra/dz7/functions_phonebook.py
@@ -1,10 +1,10 @@
 import sys
 def addition():
-     modified_contact = input("Напишите имя!: ")
-     modified_contact = modified_contact+" "+input("Напишите телефон: ")
-     modified_contact = modified_contact+" "+input("Напишите комментарий: ")+'\n'
+     new_contact = input("Напишите имя!: ")
+     new_contact = new_contact+" "+input("Напишите телефон: ")
+     new_contact = new_contact+" "+input("Напишите комментарий: ")+'\n'
      with open("phone_book.txt", 'a') as phone_book_arg:
-            phone_book_arg.write(modified_contact)
+            phone_book_arg.write(new_contact)
 def find_contact(argument = None):
       if argument is None:
             with open("phone_book.txt", 'r') as phone_book_arg:
@@ -29,7 +29,7 @@ def find_contact(argument = None):
                   print("Error!")
                   cursor = input()
             if cursor == "modify":
-                  modify(contact_list,criteria)
+                  modify_contact(contact_list,criteria)
             elif cursor == "exit":
                   sys.exit("Good Bye!")
             elif cursor == "back":
@@ -41,7 +41,7 @@ def find_contact(argument = None):
                   print("Error!")
                   cursor = input()      
             if cursor == "unite":
-                  find_contact(unite(contact_list))
+                  find_contact(unite_copies(contact_list))
             elif cursor == "find":
                   find_contact()
             elif cursor == "exit":
@@ -49,27 +49,24 @@ def find_contact(argument = None):
 
 
                         
-def modify(contact_list_arg, criteria_arg):
-    modified_contact = []
-    for row in contact_list_arg:
-        if criteria_arg in row:
-            modified_contact.append(row)
-            contact_list_arg.remove(row)
-    modified_contact[0] = input("Напишите новое имя!: ")
-    modified_contact[1] = input("Напишите новый телефон: ")
-    modified_contact[2] = input("Напишите новый комментарий: ")
-    contact_list_arg.append(modified_contact)
-    print(contact_list_arg)
+def modify_contact(contact_list_arg, criteria_arg):
+      for row in range(len(contact_list_arg)):
+            if criteria_arg in contact_list_arg[row]:                
+                  contact_list_arg[row][0] = input("Напишите новое имя!: ")
+                  contact_list_arg[row][1] = input("Напишите новый телефон: ")
+                  contact_list_arg[row][2] = input("Напишите новый комментарий: ")
+      print(contact_list_arg)
       
                   
       
+def delete_contact(contact_list_arg, criteria_arg):
 
 
 
                  
                        
-def unite(contact_list_arg):
-      contact_list_arg=set(map(tuple, contact_list_arg))
+def unite_copies(contact_list_arg):
+      contact_list_arg=set(map(tuple, contact_list_arg))#Списки не хэшируются, нужно сперва переводить в кортежи
       contact_list_arg = [list(line) for line in contact_list_arg]
       return contact_list_arg         
 
